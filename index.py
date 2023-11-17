4f52cfdd16f4ba788ef4a436f5cd4f697b23fbaf
Igor Maciel <124538209+Igoreleven@users.noreply.github.com>
2023-11-17 19:52:58 -0300

    Calculadora_1.0

--- index.py
+++ index.py
@@ -1,42 +1,47 @@
 from tkinter import *
 from tkinter import ttk
 
+co1 = "#feffff"
+co2 = "#38576b"
+co3 = "#ECEFF1"
+co4 = '#fc9303'
 
-co1 = "#feffff" 
-co2 = "#38576b"  
-co3 ="#ECEFF1"
-co4='#FFAB40' 
+# "#FFAB40"
 
-fundo = "#3b3b3b" 
- 
+fundo = "#3b3b3b"
+
 janela = Tk()
 janela.title('')
 janela.geometry('235x318')
 janela.configure(bg=co1)
- 
+
 ttk.Separator(janela, orient=HORIZONTAL).grid(row=0, columnspan=1, ipadx=280)
- 
-frame_tela = Frame(janela, width=300, height=56,bg=co2, pady=0, padx=0, relief="flat",)
+
+frame_tela = Frame(janela, width=300, height=56, bg=co2, pady=0, padx=0, relief="flat", )
 frame_tela.grid(row=1, column=0, sticky=NW)
- 
-frame_quadros = Frame(janela, width=300, height=340,bg=fundo, pady=0, padx=0, relief="flat",)
+
+frame_quadros = Frame(janela, width=300, height=340, bg=fundo, pady=0, padx=0, relief="flat", )
 frame_quadros.grid(row=2, column=0, sticky=NW)
 
-app_tela = Label(frame_tela,text='',width=16,height=2, padx=7, relief="flat", anchor="e",bd=0, justify=RIGHT, font=('Ivy 18 '), bg='#37474F', fg=co1)
-app_tela.place(x=0, y=0)
-
-todos_valores = "" 
- 
 valor_texto = StringVar()
 
+app_tela = Label(frame_tela,textvariable=valor_texto, width=16,height=2, padx=7, relief="flat", anchor="e",bd=0, justify=RIGHT, font=('Ivy 18 '), bg='#37474F', fg=co1)
+app_tela.place(x=0, y=0)
 
-b_1 = Button(frame_quadros,command = lambda: entrar_valor('%'), text="C", width=11, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
+
+def limpar_tela():
+    global todos_valores
+    todos_valores = ""
+    valor_texto.set("0")
+
+
+
+b_1 = Button(frame_quadros,command = limpar_tela, text="C", width=11, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
-b_2 = Button(frame_quadros,command = lambda: entrar_valor('%'), text="%", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
+b_2 = Button(frame_quadros,command = lambda: entrar_valor('%'), text="%", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_2.place(x=118, y=0)
 b_3 = Button(frame_quadros,command = lambda: entrar_valor('/'), text="/", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_3.place(x=177, y=0)
- 
 b_4 = Button(frame_quadros,command = lambda: entrar_valor('7'), text="7", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_4.place(x=0, y=52)
 b_5 = Button(frame_quadros,command = lambda: entrar_valor('8'), text="8", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
@@ -45,7 +50,6 @@ b_6 = Button(frame_quadros,command = lambda: entrar_valor('9'), text="9", width=
 b_6.place(x=118, y=52)
 b_7 = Button(frame_quadros,command = lambda: entrar_valor('*'), text="*", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_7.place(x=177, y=52)
- 
 b_8 = Button(frame_quadros,command = lambda: entrar_valor('4'), text="4", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_8.place(x=0, y=104)
 b_9 = Button(frame_quadros,command = lambda: entrar_valor('5'), text="5", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
@@ -54,7 +58,6 @@ b_10 = Button(frame_quadros,command = lambda: entrar_valor('6'), text="6", width
 b_10.place(x=118, y=104)
 b_11 = Button(frame_quadros,command = lambda: entrar_valor('-'), text="-", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_11.place(x=177, y=104)
- 
 b_12 = Button(frame_quadros,command = lambda: entrar_valor('1'), text="1", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_12.place(x=0, y=156)
 b_13 = Button(frame_quadros,command = lambda: entrar_valor('2'), text="2", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
@@ -63,38 +66,29 @@ b_14 = Button(frame_quadros,command = lambda: entrar_valor('3'), text="3", width
 b_14.place(x=118, y=156)
 b_15 = Button(frame_quadros,command = lambda: entrar_valor('+'), text="+", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_15.place(x=177, y=156)
- 
 b_16 = Button(frame_quadros,command = lambda: entrar_valor('0'), text="0", width=11, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_16.place(x=0, y=208)
 b_17 = Button(frame_quadros,command = lambda: entrar_valor('.'), text=".", width=5, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_17.place(x=118, y=208)
-b_18 = Button(frame_quadros,command = lambda: entrar_valor('='), text="=", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
-
-b_18 = Button(frame_quadros,command = lambda: calcular(), text="=", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
+b_18 = Button(frame_quadros,command = lambda: resultado(''), text="=", width=5, height=2, bg=co4, fg=co1,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
 b_18.place(x=177, y=208)
 
-b_1 = Button(frame_quadros,command = lambda: limpar_tela(), text="C", width=11, height=2, bg=co3, fg=fundo,font=('Ivy 13 bold'),relief=RAISED, overrelief=RIDGE)
-b_1.place(x=0, y=0)
+todos_valores = ''
 
+print(todos_valores)
 
-def limpar_tela(): 
-    global todos_valores
-    todos_valores = "" 
-    valor_texto.set("")
-
-def calcula():
-    a =eval('9/9')
 
 def entrar_valor(event):
     global todos_valores
- 
     todos_valores = todos_valores + str(event)
+    resultado = eval('9/9')
     valor_texto.set(todos_valores)
 
-def calcular():
+def resultado(event):
     global todos_valores
-    resultado = str(eval(todos_valores))
-    valor_texto.set(resultado)
-    todos_valores = ""
+    resultado = eval(todos_valores)
+    valor_texto.set(str(resultado)) 
+
+#tamo junto
 
 janela.mainloop()
